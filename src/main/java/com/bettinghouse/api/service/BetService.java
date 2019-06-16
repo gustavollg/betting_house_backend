38df600a098a874c01818286e3ad6306472b9945
gustavollg <gustavolinhares1995@gmail.com>
2019-06-17 00:03:16 +0100

    Implement Remove coins after bet

--- src/main/java/com/bettinghouse/api/service/BetService.java
+++ src/main/java/com/bettinghouse/api/service/BetService.java
@@ -6,6 +6,7 @@ import com.bettinghouse.api.controller.dto.BetDTO;
 import com.bettinghouse.api.model.Bet;
 import com.bettinghouse.api.model.User;
 import com.bettinghouse.api.repository.BetRepository;
+import com.bettinghouse.api.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,20 +14,25 @@ import org.springframework.stereotype.Service;
 public class BetService extends CRUDService<Bet> {
     
     private BetRepository betRepository;
+    private UserRepository userRepository;
 
     @Autowired
-    public BetService(BetRepository betRepository) {
+    public BetService(BetRepository betRepository, UserRepository userRepository) {
         super(betRepository);
         this.betRepository = betRepository;
+        this.userRepository = userRepository;
     }
     
     public Bet saveBet(BetDTO betDTO) {
-        User user = AuthenticationUtil.getAuthenticatedUser().get();
+        User userAuthenticated = AuthenticationUtil.getAuthenticatedUser().get();
+        User user = userRepository.findById(userAuthenticated.getId()).get();
         Bet bet = new Bet();
         bet.setBet(betDTO.getBet());
         bet.setOdd(betDTO.getOdd());
         bet.setEvent(betDTO.getEvent());
         bet.setUser(user);
+        user.setCoins(user.getCoins() - bet.getBet());
+        userRepository.save(user);
         return betRepository.save(bet);
     }
 
